6e0c367fadd18cf5f0efedbd2cd8f7c63e2516c4
Samir Akre <sakre@ucdavis.edu>
2019-01-10 13:45:35 -0800

    Made two columns instead of two rows, added subject line to email anchor tag

--- src/components/contactPage.jsx
+++ src/components/contactPage.jsx
@@ -8,17 +8,26 @@ function ContactPage() {
           <h2 className="light">Contact the BIC</h2>
         </div>
       </div>
-      <div className="row pt-3">
-        <div className="col-12">
-          <h5 className="heavy">Accessing Data: <a href="mailto:MoTrPAC-data-requests@xxx.xxx" target="_new"> MoTrPAC-data-requests@xxx.xxx</a></h5>
+      <div className="row pt-3 justify-content-center mt-5">
+        <div className="col-5 mx-3 contactCol">
+          <h5 className="heavy">
+            Accessing Data:
+            <br />
+            <a href="mailto:motrpac-data-requests@xxx.xxx?subject=Data Access Help" target="_new"> motrpac-data-requests@xxx.xxx</a>
+          </h5>
           <p>
-            Data generated to date is not yet publicly accessible. 
+            Data generated to date is not yet publicly accessible.
           </p>
           <p>
             For updates when publicly accessible data are available, please contact us at the email above.
           </p>
-
-          <h5 className="heavy">Uploading Data From Study Sites: <a href="mailto:MoTrPAC-helpdesk@xxx.xxx" target="_new"> MoTrPAC-helpdesk@xxx.xxx</a></h5>
+        </div>
+        <div className="col-5 mx-3 contactCol">
+          <h5 className="heavy">
+            Uploading Data From Study Sites:
+            <br />
+            <a href="mailto:motrpac-helpdesk@xxx.xxx?subject=Data Upload Help" target="_new"> motrpac-helpdesk@xxx.xxx</a>
+          </h5>
 
           <p>
           If you are a member of one of the sites involved with MoTrPAC please log in using
