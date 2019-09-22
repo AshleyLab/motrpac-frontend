dedd76d4bbb4f044b8199a2010e3bcd1157bf6a8
Jimmy Zhen <jzhen@stanford.edu>
2019-09-21 19:29:13 -0700

    Implemented Auth0 higher order component for protected routes
    As part of the Auth0 refactoring, the Callback route has been removed. Furthermore, some routes no longer need to be connected components due to the removal of their Redux state dependencies.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,8 +5,8 @@ import configureStore from './configureStore';
 import 'bootstrap';
 import History from './history';
 import NavbarConnected from '../Navbar/navbar';
-import FooterConnected from '../Footer/footer';
-import LandingPageConnected from '../LandingPage/landingPage';
+import Footer from '../Footer/footer';
+import LandingPage from '../LandingPage/landingPage';
 import DashboardConnected from '../Dashboard/dashboard';
 import UploadScreenConnected from '../UploadPage/uploadScreen';
 import LinkoutPageConnected from '../LinkoutPage/linkoutPage';
@@ -17,15 +17,23 @@ import TeamPageConnected from '../TeamPage/teamPage';
 import ContactConnected from '../ContactPage/contact';
 import ErrorPageConnected from '../ErrorPage/error';
 import SearchPageConnected from '../Search/searchPage';
-import ReleasePageConnected from '../ReleasePage/releasePage';
+import ReleasePage from '../ReleasePage/releasePage';
 import DataSummaryPageConnected from '../DataSummaryPage/dataSummaryPage';
-import CallbackConnected from '../Auth/callback';
 import SidebarConnected from '../Sidebar/sidebar';
 import { withTracker } from '../GoogleAnalytics/googleAnalytics';
+import PrivateRoute from '../Auth/PrivateRoute';
+import { useAuth0 } from '../Auth/Auth';
+import AnimatedLoadingIcon from '../lib/ui/loading';
 
 const store = configureStore();
 
 function App({ history = History }) {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <AnimatedLoadingIcon isFetching={loading} />;
+  }
+
   return (
     <Provider store={store}>
       <Router history={history}>
@@ -36,25 +44,24 @@ function App({ history = History }) {
           <div className="row justify-content-center">
             <SidebarConnected />
             <Switch>
-              <Route path="/callback" component={withTracker(CallbackConnected)} />
-              <Route path="/" exact component={withTracker(LandingPageConnected)} />
-              <Route path="/dashboard" component={withTracker(DashboardConnected)} />
-              <Route path="/upload" component={withTracker(UploadScreenConnected)} />
+              <Route path="/" exact component={withTracker(LandingPage)} />
+              <PrivateRoute path="/dashboard" component={withTracker(DashboardConnected)} />
+              <PrivateRoute path="/upload" component={withTracker(UploadScreenConnected)} />
               <Route path="/external-links" component={withTracker(LinkoutPageConnected)} />
-              <Route path="/download" component={withTracker(DownloadPageConnected)} />
-              <Route path="/analysis/:subjectType" component={withTracker(AnalysisHomePageConnected)} />
-              <Route path="/methods" component={withTracker(MethodsConnected)} />
+              <PrivateRoute path="/download" component={withTracker(DownloadPageConnected)} />
+              <PrivateRoute path="/analysis/:subjectType" component={withTracker(AnalysisHomePageConnected)} />
+              <PrivateRoute path="/methods" component={withTracker(MethodsConnected)} />
               <Route path="/team" component={withTracker(TeamPageConnected)} />
               <Route path="/contact" component={withTracker(ContactConnected)} />
               <Route path="/error" component={withTracker(ErrorPageConnected)} />
-              <Route path="/search" component={withTracker(SearchPageConnected)} />
-              <Route path="/summary" component={withTracker(DataSummaryPageConnected)} />
-              <Route path="/releases" component={withTracker(ReleasePageConnected)} />
+              <PrivateRoute path="/search" component={withTracker(SearchPageConnected)} />
+              <PrivateRoute path="/summary" component={withTracker(DataSummaryPageConnected)} />
+              <PrivateRoute path="/releases" component={withTracker(ReleasePage)} />
             </Switch>
           </div>
         </div>
       </Router>
-      <FooterConnected />
+      <Footer />
     </Provider>
   );
 }
