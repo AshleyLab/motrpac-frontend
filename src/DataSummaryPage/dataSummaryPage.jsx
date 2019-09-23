ee50837f947ce22d12343fc79710478c231f9c88
Jimmy Zhen <jzhen@stanford.edu>
2019-09-23 16:41:13 -0700

    :bug: Removed unused 'export' syntax

--- src/DataSummaryPage/dataSummaryPage.jsx
+++ src/DataSummaryPage/dataSummaryPage.jsx
@@ -9,7 +9,7 @@ import TissueAnalysisPlanTable from './tissueAnalysisPlanTable';
  *
  * @returns {object} JSX representation of the data summary page
  */
-export function DataSummaryPage() {
+function DataSummaryPage() {
   return (
     <div className="col-md-9 ml-sm-auto col-lg-10 px-4 dataSummaryPage">
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
