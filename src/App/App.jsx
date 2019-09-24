a44bad6926cecb59b1f5e1f82d9a96d5b44dac32
Jimmy Zhen <jzhen@stanford.edu>
2019-09-24 01:08:54 -0700

    Updated routes for components that are no longer connected to Redux states
    And reordered imports and routes in groups of non-connected and connected

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,22 +4,22 @@ import { Provider } from 'react-redux';
 import configureStore from './configureStore';
 import 'bootstrap';
 import History from './history';
-import NavbarConnected from '../Navbar/navbar';
 import Footer from '../Footer/footer';
 import LandingPage from '../LandingPage/landingPage';
+import TeamPage from '../TeamPage/teamPage';
+import LinkoutPage from '../LinkoutPage/linkoutPage';
+import Contact from '../ContactPage/contact';
+import ErrorPage from '../ErrorPage/error';
+import Methods from '../MethodsPage/methods';
+import DataSummaryPage from '../DataSummaryPage/dataSummaryPage';
+import ReleasePage from '../ReleasePage/releasePage';
+import NavbarConnected from '../Navbar/navbar';
+import SidebarConnected from '../Sidebar/sidebar';
 import DashboardConnected from '../Dashboard/dashboard';
 import UploadScreenConnected from '../UploadPage/uploadScreen';
-import LinkoutPageConnected from '../LinkoutPage/linkoutPage';
 import AnalysisHomePageConnected from '../AnalysisPage/analysisHomePage';
 import DownloadPageConnected from '../DownloadPage/downloadPage';
-import MethodsConnected from '../MethodsPage/methods';
-import TeamPageConnected from '../TeamPage/teamPage';
-import ContactConnected from '../ContactPage/contact';
-import ErrorPageConnected from '../ErrorPage/error';
 import SearchPageConnected from '../Search/searchPage';
-import ReleasePage from '../ReleasePage/releasePage';
-import DataSummaryPageConnected from '../DataSummaryPage/dataSummaryPage';
-import SidebarConnected from '../Sidebar/sidebar';
 import { withTracker } from '../GoogleAnalytics/googleAnalytics';
 import PrivateRoute from '../Auth/PrivateRoute';
 import { useAuth0 } from '../Auth/Auth';
@@ -45,17 +45,17 @@ function App({ history = History }) {
             <SidebarConnected />
             <Switch>
               <Route path="/" exact component={withTracker(LandingPage)} />
+              <Route path="/team" component={withTracker(TeamPage)} />
+              <Route path="/external-links" component={withTracker(LinkoutPage)} />
+              <Route path="/contact" component={withTracker(Contact)} />
+              <Route path="/error" component={withTracker(ErrorPage)} />
               <PrivateRoute path="/dashboard" component={withTracker(DashboardConnected)} />
               <PrivateRoute path="/upload" component={withTracker(UploadScreenConnected)} />
-              <Route path="/external-links" component={withTracker(LinkoutPageConnected)} />
               <PrivateRoute path="/download" component={withTracker(DownloadPageConnected)} />
               <PrivateRoute path="/analysis/:subjectType" component={withTracker(AnalysisHomePageConnected)} />
-              <PrivateRoute path="/methods" component={withTracker(MethodsConnected)} />
-              <Route path="/team" component={withTracker(TeamPageConnected)} />
-              <Route path="/contact" component={withTracker(ContactConnected)} />
-              <Route path="/error" component={withTracker(ErrorPageConnected)} />
+              <PrivateRoute path="/methods" component={withTracker(Methods)} />
               <PrivateRoute path="/search" component={withTracker(SearchPageConnected)} />
-              <PrivateRoute path="/summary" component={withTracker(DataSummaryPageConnected)} />
+              <PrivateRoute path="/summary" component={withTracker(DataSummaryPage)} />
               <PrivateRoute path="/releases" component={withTracker(ReleasePage)} />
             </Switch>
           </div>
