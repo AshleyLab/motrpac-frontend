807da4a9cda3740162b336218f3985f0dfc286f5
Jimmy Zhen <jzhen@stanford.edu>
2019-09-24 15:03:16 -0700

    Removed no-longer-used auth0 config file

--- src/Auth/auth0-variables.js
+++
@@ -1,7 +0,0 @@
-const url = new URL(window.location.href);
-
-export const AUTH0_CONFIG = {
-  domain: 'motrpac-project.auth0.com',
-  clientId: '4dUo4JxLlZvCtFVCw21Nh0ZRKyznluAZ',
-  callbackUrl: `${url.origin}/callback`
-};
